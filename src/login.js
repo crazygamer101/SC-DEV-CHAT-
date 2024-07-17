0b2b91853d2e36ff7b34f2e4bccc8acc84e85406
crazygamer101 <crazygamer101@gmail.com>
2024-07-16 21:03:45 -0400

    Login launches headless

--- src/login.js
+++ src/login.js
@@ -8,7 +8,7 @@ const COOKIES_PATH = './localData/cookies.json';
 const TARGET_URL = 'https://robertsspaceindustries.com/spectrum/community/SC/lobby/38230';
 
 async function login(){
-  const browser = await puppeteer.launch({headless: false, defaultViewport: null});
+  const browser = await puppeteer.launch({headless: true, defaultViewport: null});
   const page = await browser.newPage();
 
   const cookiesExist = fs.existsSync(COOKIES_PATH);
