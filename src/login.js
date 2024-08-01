11e7b632f6deb1b51f5ca7bf62d37dea08635fd0
crazygamer101 <crazygamer101@gmail.com>
2024-07-31 21:51:57 -0400

    refined the log in function to properly use cookies

--- src/login.js
+++ src/login.js
@@ -8,36 +8,48 @@ const COOKIES_PATH = './localData/cookies.json';
 const TARGET_URL = 'https://robertsspaceindustries.com/spectrum/community/SC/lobby/38230';
 
 async function login() {
-  const browser = await puppeteer.launch({headless: true, defaultViewport: null});
+  const browser = await puppeteer.launch({ headless: false, defaultViewport: null });
   const page = await browser.newPage();
-
-  await page.goto('https://robertsspaceindustries.com/connect?jumpto=/spectrum/community/SC/lobby/38230');
-
-  // Wait for the username input field to appear
-  await page.waitForSelector('input[data-cy-id="input"][id=":r1:"]', { visible: true });
 
   const cookiesExist = fs.existsSync(COOKIES_PATH);
   if (cookiesExist) {
     const cookies = JSON.parse(fs.readFileSync(COOKIES_PATH, 'utf8'));
     await page.setCookie(...cookies);
     console.log('Cookies used');
+  }
 
-    // Check if the page redirected correctly
-    await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
-    const currentURL = page.url();
+  await page.goto('https://robertsspaceindustries.com/connect?jumpto=/spectrum/community/SC/lobby/38230');
+
+  if (cookiesExist) {
+    let currentURL = page.url();
     if (currentURL !== TARGET_URL) {
       console.log('Cookies use failed. Current URL:', currentURL);
-      await performLogin(page);
+      await abbreviatedLogin(page);
     } else {
       console.log('Monitoring SC Testing Chat.');
-      return page;
     }
   } else {
     await performLogin(page);
   }
-  
+
   return page;
 };
+
+async function abbreviatedLogin(page) {
+  await page.type('input[data-cy-id="input"][id=":r1:"]', USERNAME);
+  await page.type('input[data-cy-id="input"][id=":r2:"]', PASSWORD);
+  await page.click('button[type="submit"][data-cy-id="__submit-button"]');
+
+  await page.waitForNavigation({ waitUntil: 'networkidle2' });
+
+  // Confirm if redirected to the desired URL
+  const currentURL = page.url();
+  if (currentURL === TARGET_URL) {
+    console.log('Monitoring SC Testing Chat.');
+  } else {
+    console.log('Did not redirect to the expected URL. Current URL:', currentURL);
+  }
+}
 
 async function performLogin(page) {
   await page.type('input[data-cy-id="input"][id=":r1:"]', USERNAME);
